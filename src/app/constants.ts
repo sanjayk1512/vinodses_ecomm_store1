b715bcbb7c0a0f8eef67b7e3727361839ca510fc
Darin40 <132036315+Darin40@users.noreply.github.com>
2025-03-15 16:42:30 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://aa8b231f0015f4d209a78d58ad391485-341218138.ap-south-1.elb.amazonaws.com',
 };
