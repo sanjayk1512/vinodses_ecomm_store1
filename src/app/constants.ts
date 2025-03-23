239c243f400d109039fc883f06029999c40d3b00
Sanjay K <2001ksanjay@gmail.com>
2025-03-23 19:15:46 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://database-1.c3kaay88gbjs.ap-south-1.rds.amazonaws.com',
+    BACKEND_API_URL: 'database-1.c3kaay88gbjs.ap-south-1.rds.amazonaws.com',
 };
